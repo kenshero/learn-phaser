45b8e7679df8d10ca9aff43237b50c963d7297e6
kenshero <kenshero.k@gmail.com>
2017-09-12 04:34:47 +0700

    add project-2

--- project-2/js/main.js
+++ project-2/js/main.js
@@ -4,10 +4,13 @@ var GameState = {
   // load the game assets before game start
   preload: function() {
     this.load.image('background', 'assets/images/background-game.jpg')
+    this.load.image('knight', 'assets/images/knight.png')
   },
   // execute aftert everything is loaded
   create: function() {
     this.background = this.game.add.sprite(0, 0, 'background')
+    this.knight = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'knight')
+    this.knight.anchor.setTo(0.5)
   },
   // this is executed multiple time per second
   update: function() {
