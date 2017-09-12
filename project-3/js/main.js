778afc2b2b3d4a02e8bd63b10f61e926b68dcf2a
kenshero <kenshero.k@gmail.com>
2017-09-13 02:56:59 +0700

    add project 3

---
+++ project-3/js/main.js
@@ -0,0 +1,28 @@
+var game = new Phaser.Game({ width: 640, height: 360, renderer: Phaser.AUTO, enableDebug: false})
+
+var GameState = {
+  // load the game assets before game start
+  preload: function() {
+    this.load.image('background', 'assets/images/background-game.jpg')
+    this.load.image('knight', 'assets/images/knight.png')
+    this.load.image('wolf', 'assets/images/fenrir_wolf.png')
+  },
+  // execute aftert everything is loaded
+  create: function() {
+    this.background = this.game.add.sprite(0, 0, 'background')
+    this.knight = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'knight')
+    this.knight.anchor.setTo(0.5)
+    this.knight.scale.setTo(0.5, 0.5)
+
+    this.wolf = this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY, 'wolf')
+    this.wolf.anchor.setTo(0.5)
+    this.wolf.scale.setTo(-1, -1)
+  },
+  // this is executed multiple time per second
+  update: function() {
+
+  }
+}
+
+game.state.add('GameState', GameState)
+game.state.start('GameState')
