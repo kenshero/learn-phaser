e349686f227ff3b3552a1138e7627202fa10c65f
kenshero <kenshero.k@gmail.com>
2017-09-14 04:01:49 +0700

    add phaserDemo udemy learn

---
+++ phaserDemo/state0.js
@@ -0,0 +1,43 @@
+var demo = {};
+demo.state0 = function(){};
+
+demo.state0.prototype = {
+  preload: function(){
+
+  },
+  create: function(){
+    console.log("state0");
+    game.stage.backgroundColor = '#3fcc6e'
+    initialKeyListener()
+  },
+  update: function(){
+
+  }
+}
+
+function changeState(i, stateNum){
+  console.log(i);
+  game.state.start('state'+ stateNum)
+}
+
+function addKeyListener(key, fn, state){
+  game.input.keyboard.addKey(key).onDown.add(
+    fn,
+    null,
+    null,
+    state
+  )
+}
+
+function initialKeyListener(){
+  addKeyListener(Phaser.Keyboard.ZERO, changeState, 0)
+  addKeyListener(Phaser.Keyboard.ONE, changeState, 1)
+  addKeyListener(Phaser.Keyboard.TWO, changeState, 2)
+  addKeyListener(Phaser.Keyboard.THREE, changeState, 3)
+  addKeyListener(Phaser.Keyboard.FOUR, changeState, 4)
+  addKeyListener(Phaser.Keyboard.FIVE, changeState, 5)
+  addKeyListener(Phaser.Keyboard.SIX, changeState, 6)
+  addKeyListener(Phaser.Keyboard.SEVEN, changeState, 7)
+  addKeyListener(Phaser.Keyboard.EIGHT, changeState, 8)
+  addKeyListener(Phaser.Keyboard.NINE, changeState, 9)
+}
