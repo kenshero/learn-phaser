b15555fc4350402a42c20e1730af5e29380761ed
kenshero <kenshero.k@gmail.com>
2017-09-19 02:28:44 +0700

    add phaser-10 tween function

---
+++ phaserDemo-10/state9.js
@@ -0,0 +1,14 @@
+demo.state9 = function(){};
+
+demo.state9.prototype = {
+  preload: function(){
+
+  },
+  create: function(){
+    game.stage.backgroundColor = '#126323'
+    initialKeyListener()
+  },
+  update: function(){
+
+  }
+}
